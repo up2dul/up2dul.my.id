79718d571553741fb8f5558f62a8ba0d51ad3cfc
abdul <malik301002@gmail.com>
2022-11-10 20:00:48 +0700

    chore: add custom fontSize and colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: { inter: ['Inter', 'sans-serig'] }
+    extend: {
+      fontSize: {
+        'h1': '40px',
+        'h2': '24px',
+        'h3': '18px'
+      }
+    },
+    fontFamily: { inter: ['Inter', 'sans-serif'] },
+    colors: {
+      black: '#161616',
+      white: '#F2F2F2',
+      blue: '#6358EE',
+      purple: '#965DE9',
+      transparent: colors.transparent
+    }
   },
   plugins: []
 };
