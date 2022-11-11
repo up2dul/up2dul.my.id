a958e28b952b5524aae73595af46653bdaf50b3d
abdul <malik301002@gmail.com>
2022-11-11 19:52:26 +0700

    feat(pages): add Navbar component and temporary content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import Navbar from '@/components/Navbar';
+
 const Home = () => {
   return (
     <>
@@ -7,7 +9,18 @@ const Home = () => {
         <title>up2dul&apos;s site</title>
       </Head>
 
-      <h1 className='text-teal-600'>Hello next js</h1>
+      <Navbar />
+
+      <div className='px-container mt-5 flex flex-col items-center justify-center'>
+        <h1 className='text-gradient'>Hello next js</h1>
+        <p className='text-center'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.{' '}
+          <span className='text-gradient'>Molestias</span> illo obcaecati fugiat quaerat impedit
+          maxime tempora sequi quos. Illum, eos!
+        </p>
+        <h2 className='text-gradient'>Hello next js</h2>
+        <h3 className='text-gradient'>Hello next js</h3>
+      </div>
     </>
   );
 };
