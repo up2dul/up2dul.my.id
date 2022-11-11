ac97dc22d1c6b827fa6134077bc716e3780c556c
abdul <malik301002@gmail.com>
2022-11-11 19:51:15 +0700

    feat(comps): finish the toggle theme functionality on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,29 @@
-import { FiMenu, FiSun } from 'react-icons/fi';
+import { useTheme } from 'next-themes';
+import { FiMenu, FiMoon, FiSun } from 'react-icons/fi';
+
+import { getTime } from '@/utils/misc';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  const currentTime: string = getTime();
+  const toggleTheme = (): void => (theme === 'dark' ? setTheme('light') : setTheme('dark'));
+
   return (
-    <nav className='px-container flex w-full flex-col items-center justify-between gap-4 bg-black py-7 sm:flex-row'>
-      <h3>15:31 (UTC +07:00)</h3>
+    <nav className='px-container flex w-full flex-col items-center justify-between gap-4 py-7 sm:flex-row'>
+      <h3>{currentTime} (UTC +07:00)</h3>
 
-      <div className='flex items-center gap-14'>
-        <button>
-          <FiSun className='text-3xl' />
-        </button>
+      <div className='flex items-center gap-14 text-3xl'>
+        <button onClick={toggleTheme}>{theme === 'dark' ? <FiSun /> : <FiMoon />}</button>
 
         <button>
-          <FiMenu className='text-3xl' />
+          <FiMenu />
         </button>
       </div>
     </nav>
