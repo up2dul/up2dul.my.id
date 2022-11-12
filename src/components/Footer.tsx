f81f709f92cb2aca67f6ef15970ff9124f680c7a
abdul <malik301002@gmail.com>
2022-11-12 08:13:03 +0700

    feat(comps): create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => (
+  <footer className='flex flex-col flex-wrap justify-between gap-4 py-6 px-14 text-center sm:flex-row md:px-16 lg:px-20'>
+    <p className='text-sm'>Design & Code with 💙</p>
+
+    <p className='text-sm'>
+      Website repository on <a href='https://github.com/up2dul'>Github</a>
+    </p>
+
+    <p className='text-sm'>© 2022 • Abdul Malik</p>
+  </footer>
+);
+
+export default Footer;
