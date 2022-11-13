3fb9dc66b23f8c2a57b822b009d376994f5853a6
abdul <malik301002@gmail.com>
2022-11-13 21:09:39 +0700

    refactor(pages): move all meta tags to _document, and clean up code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,13 @@
-import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import 'tippy.js/dist/tippy.css';
 
 import '@/styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <ThemeProvider defaultTheme='dark'>
-      <Head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='msapplication-TileColor' content='#965de9' />
-        <meta name='theme-color' content='#f2f2f2' />
-        <meta name='author' content='Abdul malik (up2dul)' />
-        <meta name='title' content="up2dul's site" />
-        <meta
-          name='description'
-          content='My personal website, a website where I showcase some my projects, contacts, etc.'
-        />
-      </Head>
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider defaultTheme='dark'>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-};
-
-export default MyApp;
+export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,17 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => (
   <Html lang='en'>
     <Head>
+      <meta name='msapplication-TileColor' content='#965de9' />
+      <meta name='theme-color' content='#f2f2f2' />
+      <meta name='author' content='Abdul malik (up2dul)' />
+      <link rel='shortcut icon' href='/favicon.ico' />
       <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
       <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
       <link rel='manifest' href='/site.webmanifest' />
       <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#965de9' />
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap'
-        rel='stylesheet'
-      />
     </Head>
 
     <body>
