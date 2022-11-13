cd428beb128071e9530d386d0eceeac242734711
abdul <malik301002@gmail.com>
2022-11-13 21:23:29 +0700

    refactor(comps): modify main element structure and edit height
    wrap props.children to section element, and then wrap with main along
    with Footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,11 +25,13 @@ const Layout = ({ title, description, children }: LayoutProps) => {
 
       <Navbar />
 
-      <main className='px-container mt-[97px] flex flex-col items-center justify-center gap-7 py-24 text-center sm:mt-[62px]'>
-        {children}
-      </main>
+      <main className='flex min-h-screen flex-col justify-between pt-[97px] sm:pt-[62px]'>
+        <section className='px-container gap-7py-14 my-auto flex flex-col items-center justify-center text-center'>
+          {children}
+        </section>
 
-      <Footer />
+        <Footer />
+      </main>
     </>
   );
 };
