dab990d4f5bc20f0898256dee97c905b651d18f7
abdul <malik301002@gmail.com>
2022-11-13 21:16:14 +0700

    feat(comps): add reusable meta title and description to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,28 @@
+import Head from 'next/head';
+
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+type LayoutProps = {
+  title?: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+const Layout = ({ title, description, children }: LayoutProps) => {
+  const metaTitle = title ? title + ' | ' + "up2dul's site" : "up2dul's site";
+  const metaDescription =
+    description ||
+    'My personal website, a website where I showcase some my projects, contacts, etc.';
+
   return (
     <>
+      <Head>
+        <meta name='title' content={metaTitle} />
+        <meta name='description' content={metaDescription} />
+        <title>{metaTitle}</title>
+      </Head>
+
       <Navbar />
 
       <main className='px-container mt-[97px] flex flex-col items-center justify-center gap-7 py-24 text-center sm:mt-[62px]'>
