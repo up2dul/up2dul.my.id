d0b6526a055a2c87aaa7399e398cd783ac3dda62
abdul <malik301002@gmail.com>
2022-11-13 16:00:25 +0700

    feat(comps): add temporary Link on te list menu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,23 +1,29 @@
+import Link from 'next/link';
 import { FiX } from 'react-icons/fi';
+
+import Tooltip from './Tooltip';
 
 const NavMenu = ({ onCloseClick }: { onCloseClick: () => void }) => (
   <aside className='fixed top-0 z-10 flex h-screen w-full items-center justify-center bg-black/10 backdrop-blur-md'>
-    <button
-      className='absolute right-6 top-8 text-5xl sm:right-20 md:right-40 lg:right-56 xl:right-72'
-      onClick={onCloseClick}
-    >
-      <FiX />
-    </button>
+    <Tooltip content='Close'>
+      <button
+        className='absolute right-6 top-8 text-5xl sm:right-20 md:right-40 lg:right-56 xl:right-72'
+        onClick={onCloseClick}>
+        <FiX />
+      </button>
+    </Tooltip>
 
     <ul className='text-center'>
-      <li>
-        <h1>Home</h1>
+      <li className='text-h1 decoration-blue decoration-4 underline-offset-4 hover:text-blue hover:underline'>
+        <Link href={'/'}>Home</Link>
       </li>
-      <li>
-        <h1>About</h1>
+
+      <li className='text-h1 decoration-blue decoration-4 underline-offset-4 hover:text-blue hover:underline'>
+        <Link href={'/projects'}>Projects</Link>
       </li>
-      <li>
-        <h1>Projects</h1>
+
+      <li className='text-h1 decoration-blue decoration-4 underline-offset-4 hover:text-blue hover:underline'>
+        <Link href={'/about'}>About me</Link>
       </li>
     </ul>
   </aside>
