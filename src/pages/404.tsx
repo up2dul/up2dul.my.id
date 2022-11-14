5d92c15de1fc53ae3978d8dbfa098b2099bbc5d9
abdul <malik301002@gmail.com>
2022-11-14 21:01:28 +0700

    feat(pages): create 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+import { Layout } from '@/components';
+
+const NotFound = () => (
+  <Layout title='404 Page not found'>
+    <h1 className='text-4xl sm:text-h1'>
+      🧐 <span className='text-gradient'>404</span> - Looks like you&apos;re lost.
+    </h1>
+
+    <p>
+      Maybe this page used to exist or you just spelled something wrong. Chances are you spelled
+      something wrong, so can you double check the URL?
+    </p>
+
+    <Link href={'/'} className='text-link'>
+      Back to Home page.
+    </Link>
+  </Layout>
+);
+
+export default NotFound;
