c52c7c8f87350ce91168fcbbd542df945131a45a
abdul <malik301002@gmail.com>
2022-11-15 16:41:22 +0700

    feat(types): create types/index.ts files

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+type ContactList = {
+  title: string;
+  url: string;
+  icon: React.ReactNode;
+};
+
+type NavLinkList = {
+  href: string;
+  title: string;
+  isActive: boolean;
+};
+
+export type { ContactList, NavLinkList };
