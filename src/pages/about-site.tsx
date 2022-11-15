7949932fab1064f62c9154b8f823a70bb2ee35a6
abdul <malik301002@gmail.com>
2022-11-15 21:26:02 +0700

    feat(pages): about-site and blog temporary content

---
+++ src/pages/about-site.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@/components';
+
+const AboutSite = () => {
+  return (
+    <Layout title='About this site' description='Tools and other information about this website'>
+      <h1>
+        🌐 About this <span className='text-gradient'>website</span>
+      </h1>
+    </Layout>
+  );
+};
+
+export default AboutSite;
---
+++ src/pages/blog.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@/components';
+
+const Blog = () => {
+  return (
+    <Layout title='Blog' description='A place where I write anything'>
+      <h1>
+        📝 <span className='text-gradient'>Blog</span>
+      </h1>
+    </Layout>
+  );
+};
+
+export default Blog;
