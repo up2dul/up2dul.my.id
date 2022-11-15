455e9b931a56fe3b9015a0b8442ea0ba9a23be57
abdul <malik301002@gmail.com>
2022-11-15 16:45:12 +0700

    feat(comps): wrap NavLink in the ul with navLinkList data

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,33 +1,30 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FiX } from 'react-icons/fi';
 
+import { navLinkList } from '@/utils/misc';
+import NavLink from './NavLink';
 import Tooltip from './Tooltip';
 
-const NavMenu = ({ onCloseClick }: { onCloseClick: () => void }) => (
-  <aside className='fixed top-0 z-10 flex h-screen w-full items-center justify-center bg-dark/10 backdrop-blur-md'>
-    <Tooltip content='Close'>
-      <button
-        className='absolute right-6 top-8 text-5xl sm:right-20 md:right-40 lg:right-56 xl:right-72'
-        onClick={onCloseClick}
-      >
-        <FiX />
-      </button>
-    </Tooltip>
+const NavMenu = ({ onCloseClick }: { onCloseClick: () => void }) => {
+  const { pathname } = useRouter();
 
-    <ul className='text-center'>
-      <li className='text-h1 decoration-secondary decoration-4 underline-offset-4 hover:text-secondary hover:underline'>
-        <Link href={'/'}>Home</Link>
-      </li>
+  return (
+    <aside className='fixed top-0 z-10 flex h-screen w-full items-center justify-center backdrop-blur-md'>
+      <Tooltip content='Close'>
+        <button
+          className='absolute right-6 top-8 text-5xl sm:right-20 md:right-40 lg:right-56 xl:right-72'
+          onClick={onCloseClick}
+        >
+          <FiX />
+        </button>
+      </Tooltip>
 
-      <li className='text-h1 decoration-secondary decoration-4 underline-offset-4 hover:text-secondary hover:underline'>
-        <Link href={'/projects'}>Projects</Link>
-      </li>
-
-      <li className='text-h1 decoration-secondary decoration-4 underline-offset-4 hover:text-secondary hover:underline'>
-        <Link href={'/about'}>About me</Link>
-      </li>
-    </ul>
-  </aside>
-);
-
+      <ul className='text-center'>
+        {navLinkList.map(({ title, href }) => (
+          <NavLink key={href} href={href} title={title} isActive={href === pathname} />
+        ))}
+      </ul>
+    </aside>
+  );
+};
 export default NavMenu;
