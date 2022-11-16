1e860c23de5ae84b7fc69b2f6435bd691adf9362
abdul <malik301002@gmail.com>
2022-11-17 00:21:46 +0700

    feat(pages): add Card with dummy props

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,16 +1,38 @@
-import { Layout } from '@/components';
+import { Layout, Card } from '@/components';
 
 const Projects = () => {
   return (
     <Layout title='Projects' description='Some of my projects showcase'>
-      <div>
-        <h1>
-          🎨 My <span className='text-gradient'>projects</span>
-        </h1>
+      <h1>
+        🎨 My <span className='text-gradient'>projects</span>
+      </h1>
 
-        <p className='mt-5 text-base'>
-          Some of my latest projects, currently there is only website project.
-        </p>
+      <p className='mt-5 text-base'>
+        Some of my latest projects, currently there is only website project.
+      </p>
+
+      <div className='mt-14 grid w-full grid-cols-1 gap-12 lg:grid-cols-2'>
+        <Card
+          href='#'
+          order={1}
+          title='Bookshelf'
+          description='Manage your reading list and your wishlist books'
+          technologies={['Next js', 'Mantine UI', 'Typescript', 'Framer motion']}
+        />
+        <Card
+          href='#'
+          order={2}
+          title='ToDo list'
+          description='An app for your productivity, notes your activity'
+          technologies={['Vite', 'Tailwindcss', 'Jotai']}
+        />
+        <Card
+          href='#'
+          order={3}
+          title='ToDo list'
+          description='An app for your productivity'
+          technologies={['Vite', 'Tailwindcss', 'Jotai']}
+        />
       </div>
     </Layout>
   );
