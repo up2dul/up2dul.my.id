3984f993e4fa6e43a0ddf5fc51b3c75fc3b18b97
abdul <malik301002@gmail.com>
2022-11-18 14:31:25 +0700

    feat(utils): add 3 letters day to getTime

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,10 +2,14 @@ function padTo2Digits(num: number) {
   return String(num).padStart(2, '0');
 }
 
+const days: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const d = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }));
 
 function getTime() {
-  return padTo2Digits(d.getHours()) + ':' + padTo2Digits(d.getMinutes());
+  const day: string = days[d.getDay()];
+  const hoursMinutes: string = padTo2Digits(d.getHours()) + ':' + padTo2Digits(d.getMinutes());
+
+  return `${day}, ${hoursMinutes}`;
 }
 
 export { getTime };
