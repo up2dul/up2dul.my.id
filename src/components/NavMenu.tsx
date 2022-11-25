885c4f7793c84a73daab591b4e480cf57bd549bc
abdul <malik301002@gmail.com>
2022-11-25 11:25:20 +0700

    feat(comps): use useEscPress to Navbar and NavMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
-import { FiX } from 'react-icons/fi';
+import { FiAlertCircle, FiX } from 'react-icons/fi';
 
+import { useEscPress } from '@/utils/hooks';
 import { navLinkList } from '@/utils/misc';
 import NavLink from './NavLink';
 import Tooltip from './Tooltip';
@@ -8,8 +9,13 @@ import Tooltip from './Tooltip';
 const NavMenu = ({ onCloseClick }: { onCloseClick: () => void }) => {
   const { pathname } = useRouter();
 
+  const handleEscClick = useEscPress(onCloseClick);
+
   return (
-    <aside className='fixed top-0 z-40 flex h-screen w-full items-center justify-center backdrop-blur-md'>
+    <aside
+      className='fixed top-0 z-40 flex h-screen w-full items-center justify-center backdrop-blur-md'
+      onKeyDown={handleEscClick}
+    >
       <Tooltip content='Close'>
         <button
           className='absolute right-6 top-8 text-5xl sm:right-20 md:right-40 lg:right-56 xl:right-72'
@@ -24,7 +30,13 @@ const NavMenu = ({ onCloseClick }: { onCloseClick: () => void }) => {
           <NavLink key={href} href={href} title={title} isActive={href === pathname} />
         ))}
       </ul>
+
+      <div className='absolute bottom-8 flex items-center gap-2'>
+        <FiAlertCircle />
+        <p>You can use esc to close this</p>
+      </div>
     </aside>
   );
 };
+
 export default NavMenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 
+import { useEscPress } from '@/utils/hooks';
 import { getTime } from '@/utils/helpers';
 import ButtonTheme from './ButtonTheme';
 import NavMenu from './NavMenu';
@@ -12,11 +13,13 @@ const Navbar = () => {
 
   useEffect(() => setMounted(true), []);
 
+  const handleEscClick = useEscPress(() => setIsMenuShow(false));
+
   if (!mounted) return null;
 
   return (
     <>
-      <nav className='navbar'>
+      <nav className='navbar' onKeyDown={handleEscClick}>
         <h3>{currentTime} (UTC +07:00)</h3>
 
         <div className='flex w-full items-center justify-evenly gap-14 text-3xl sm:w-auto'>
